ebc4377935225503815172de2a6366948c867a01
Bernadette Angelice Novena Octaviani <belajarcodingangelicenovenaoct@gmail.com>
2025-12-25 00:31:06 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Script sederhana untuk profil Angelice
+const user = {
+    name: "Angelice Novena",
+    status: "Active Learning",
+    tools: ["Lenovo ThinkPad", "Samsung"]
+};
+
+function greeting() {
+    console.log(`Halo, saya ${user.name} sedang belajar JavaScript.`);
+}
+
+greeting();
